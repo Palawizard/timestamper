514b93f05f005e36a856f5f52392b4c7e5701027
Palawi <bapcoloj@gmail.com>
2026-06-01 14:12:55 +0200

    chore(hotkeys): configure global shortcut plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -48,6 +48,7 @@ pub fn run() {
     }];
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(
             tauri_plugin_sql::Builder::new()
                 .add_migrations("sqlite:timestamper.db", migrations)
